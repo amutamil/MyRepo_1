745679568f771add6a27e0cc6bea834bad3b2004
amutamil <68475039+amutamil@users.noreply.github.com>
2020-09-12 18:38:05 +0530

    Update sender.cpp

--- sender/sender.cpp
+++ sender/sender.cpp
@@ -6,9 +6,9 @@ using namespace std;
 
 int main(int num,char* str[])
 {
-  string word;
+  	string word;
 	fstream file;
 	file.open(str[1], ios::in);
 	getline(file, word);
-	cout << word << endl;
+	cout << word << endl<<num<<endl;
 }
