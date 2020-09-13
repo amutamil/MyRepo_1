057f98e1330f242a95fe7eef55a32923b5cf227b
amutamil <68475039+amutamil@users.noreply.github.com>
2020-09-13 16:44:13 +0530

    Update receiver.cpp

--- receiver/receiver.cpp
+++ receiver/receiver.cpp
@@ -2,21 +2,10 @@
 #include<fstream>
 #include<vector>
 #include<sstream>
+#include "functions.h"
 using namespace std;
-bool checkExceptions(string s);
+using namespace functions;
 
-bool checkExceptions(string s)
-{
-  string arr[3]={"filename not passed as argument","file cannot be opened","file is empty"};
-  for(unsigned int i=0;i<3;i++)
-  {
-    if(s==arr[i])
-    {
-      return true;
-    }
-  }
-  return false;
-}
 
 int main()
 {
