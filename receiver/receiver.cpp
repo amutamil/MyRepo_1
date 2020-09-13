11509c0fd324453b10a2a0b7accc6839f344ec54
amutamil <68475039+amutamil@users.noreply.github.com>
2020-09-13 13:40:28 +0530

    Update receiver.cpp

--- receiver/receiver.cpp
+++ receiver/receiver.cpp
@@ -3,10 +3,22 @@
 #include<sstream>
 using namespace std;
 
+void pushIntoCsv(string str);
+
+void pushIntoCsv(string s1)
+{
+  fstream file;
+    file.open("output.csv", ios::in | ios::app);
+    file << s1<<endl;
+}
+
 int main()
 {
   string str;
-  while(getline(cin,str)){
-  cout<<str<<endl;}
+  while(getline(cin,str))
+  {
+    cout<<str<<endl;
+    pushIntoCsv(str);
+  }
   return 0;
 }
