575d2f7b54293a947150a412359b297976edaa96
amutamil <68475039+amutamil@users.noreply.github.com>
2020-09-13 22:21:09 +0530

    Update sender.cpp

--- sender/sender.cpp
+++ sender/sender.cpp
@@ -9,7 +9,7 @@ int main()
 //cout<<"filename not passed as argument"<<endl;
 cout<<endl;
 
-cout <<" 5/13/2020 15:45 change spelling"<<endl;
+cout <<"5/13/2020 15:45 change spelling"<<endl;
 
 cout<<"5/13/2020 15:50 remove this log if not required"<<endl;
 
@@ -19,5 +19,5 @@ cout<<"5/13/2020 15:51 remove this log if not required"<<endl;
 
 cout<<"5/1/2020 15:52 verify the string"<<endl;
 
-cout<<"12/12/2020 16:43,please add test cases for firebase performance"<<endl;
+cout<<"12/12/2020 16:43 please add test cases for firebase performance"<<endl;
 }
