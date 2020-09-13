0905c9cd91dc2f449695cc684a797b1f3dcefe77
amutamil <68475039+amutamil@users.noreply.github.com>
2020-09-13 12:00:35 +0530

    Update sender.cpp

--- sender/sender.cpp
+++ sender/sender.cpp
@@ -4,11 +4,17 @@
 #include<map>
 using namespace std;
 
-int main(int num,char* argv[])
-{
-  	string word;
-	fstream file;
-	file.open(argv[1], ios::in);
-	getline(file, word);
-	cout << word<<num << endl;
+int main()
+{ 
+cout <<" 5/13/2020 15:45 change spelling"<<endl;
+
+cout<<"5/13/2020 15:50 remove this log if not required"<<endl;
+
+cout<<"5/13/2020 15:51 Don't use static context"<<endl;
+
+cout<<"5/13/2020 15:51 remove this log if not required"<<endl;
+
+cout<<"5/1/2020 15:52 verify the string"<<endl;
+
+cout<<"12/12/2020 16:43,please add test cases for firebase performance"<<endl;
 }
