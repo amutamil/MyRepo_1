7ef3f6ca18c5ac7d9d63863cdb97514151cec795
amutamil <68475039+amutamil@users.noreply.github.com>
2020-09-13 21:53:22 +0530

    Update receiver.cpp

--- receiver/receiver.cpp
+++ receiver/receiver.cpp
@@ -1,23 +1,26 @@
-
 #include "functions.h"
 using namespace functions;
 
 
 int main()
 {
-  string s1;
+  string s1;    unordered_map<string, int> m;
+
   getline(cin,s1);
+  
   if(checkExceptions(s1))
   {
     cout<<s1;
     return 0;
   }
+  
   while(getline(cin,s1))
   {
      removeNums(s1);
      removeSpecialChar(s1);
      ToLowerChar(s1);
-     cout<<s1<<endl;
+     pushIntoMap(s1,m);
+     printmap(m);
   }
   return 0;
 }
