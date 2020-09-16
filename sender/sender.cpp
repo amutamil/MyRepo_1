bbc3633ee269cba7f796f64b47dce262b9c55434
sriyasrini23 <sriyasrini238@gmail.com>
2020-09-16 16:05:43 +0400

    restored files

--- sender/sender.cpp
+++ sender/sender.cpp
@@ -4,21 +4,21 @@
 #include<map>
 using namespace std;
 
-//int main()
-//{ 
-//cout<<"filename not passed as argument"<<endl;
-//cout<<endl;
-//
-//cout <<"5/13[]-2020 15:45 change s\"!@3450675pelling  "<<endl;
-// cout<<endl;
-//
-//cout<<"5/13/2020 15:50 remove this log if not required"<<endl;
-//
-//cout<<"5/13/2020 15:51 Don't use static context"<<endl;
-//
-//cout<<"5/13/2020 15:51 remove this log if not required"<<endl;
-//
-//cout<<"5/1/2020 15:52 verify the string"<<endl;
-//
-//cout<<"12/12/2020 16:43 please add test cases for firebase performance"<<endl;
-//}
+int main()
+{ 
+cout<<"filename not passed as argument"<<endl;
+cout<<endl;
+
+cout <<"5/13[]-2020 15:45 change s\"!@3450675pelling  "<<endl;
+ cout<<endl;
+
+cout<<"5/13/2020 15:50 remove this log if not required"<<endl;
+
+cout<<"5/13/2020 15:51 Don't use static context"<<endl;
+
+cout<<"5/13/2020 15:51 remove this log if not required"<<endl;
+
+cout<<"5/1/2020 15:52 verify the string"<<endl;
+
+cout<<"12/12/2020 16:43 please add test cases for firebase performance"<<endl;
+}
