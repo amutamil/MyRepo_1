6d0b62db5d5a43e762b2311a8e2e063932390851
sriyasrini23 <sriyasrini238@gmail.com>
2020-09-16 16:01:54 +0400

    do not consider

--- receiver/receiver.cpp
+++ receiver/receiver.cpp
@@ -1,30 +1,33 @@
 #include "functions.h"
+#include<iostream>
 using namespace functions;
 
 
 int main()
 {
-  string s1;    
+	std::cout << "This has been pushed!" << std::endl;
 
-  getline(cin,s1);
-  
-  if(checkExceptions(s1))
-  {
-    cout<<s1;
-    return 0;
-  }
-  
-  while(getline(cin,s1))
-  {
-     removeNums(s1);
-     removeSpecialChar(s1);
-     ToLowerChar(s1);
-     trim(s1);
-     removeStopWords(s1,m);
-      cout<<s1<<endl;
-     //pushIntoMap(s1,m);
-  }
-  printmap(m);
-  return 0;
+  //string s1;    
+
+  //getline(cin,s1);
+  //
+  //if(checkExceptions(s1))
+  //{
+  //  cout<<s1;
+  //  return 0;
+  //}
+  //
+  //while(getline(cin,s1))
+  //{
+  //   removeNums(s1);
+  //   removeSpecialChar(s1);
+  //   ToLowerChar(s1);
+  //   trim(s1);
+  //   removeStopWords(s1,m);
+  //    cout<<s1<<endl;
+  //   //pushIntoMap(s1,m);
+  //}
+  //printmap(m);
+  //return 0;
 }
 
